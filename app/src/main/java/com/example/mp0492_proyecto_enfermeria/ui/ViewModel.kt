048a56c489f36aae740919fef322d7295c3ad534
MarcLopezMolina <marclopezmolina242006@gmail.com>
2026-01-29 16:36:41 +0100

    NurseSearch endpoint connected perfectly!

--- app/src/main/java/com/example/mp0492_proyecto_enfermeria/ui/ViewModel.kt
+++ app/src/main/java/com/example/mp0492_proyecto_enfermeria/ui/ViewModel.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
 
 data class NurseUiState(
     val dynamicNurses: List<Nurse> = emptyList()
@@ -27,8 +30,36 @@ class NurseViewModel : ViewModel() {
 
     // ✅ Búsqueda (sin backend)
     fun searchNurses(query: String): List<Nurse> {
+
         if (query.isBlank()) return emptyList()
-        return getAllNurses().filter { it.name.contains(query, ignoreCase = true) }
+
+        // Lanzamos la petición en segundo plano
+        viewModelScope.launch {
+            try {
+                val response =
+                    RemoteConnection.endPoints.findByName(query)
+
+                if (response.isSuccessful && response.body() != null) {
+                    _uiState.update {
+                        it.copy(
+                            dynamicNurses = listOf(response.body()!!)
+                        )
+                    }
+                } else {
+                    _uiState.update {
+                        it.copy(dynamicNurses = emptyList())
+                    }
+                }
+            } catch (e: Exception) {
+                _uiState.update {
+                    it.copy(dynamicNurses = emptyList())
+                }
+            }
+        }
+
+        // ⚠️ IMPORTANTE:
+        // devolvemos lo que haya en el estado (Compose se recompone)
+        return uiState.value.dynamicNurses
     }
 
     fun registerNurse(name: String, user: String, email: String, password: String) {
